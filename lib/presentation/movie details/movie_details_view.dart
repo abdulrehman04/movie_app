70189c30d1a5d42ebdc25c709ba085850e4933e0
abdulrehman04 <abdulrehman71254@gmail.com>
2024-05-13 03:23:15 +0500

    completed movie dtails view

--- lib/presentation/movie details/movie_details_view.dart
+++ lib/presentation/movie details/movie_details_view.dart
@@ -1,6 +1,15 @@
+import 'package:cowlar_task/core/constants/app_colors.dart';
+import 'package:cowlar_task/core/constants/strings.dart';
 import 'package:cowlar_task/presentation/movie%20details/movie_details_vm.dart';
+import 'package:cowlar_task/widgets/button_widgets/custom_long_button.dart';
+import 'package:cowlar_task/widgets/text_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
+
+part './widgets/_app_bar.dart';
 
 class MovieDetailsView extends ConsumerStatefulWidget {
   const MovieDetailsView({super.key, required this.movieId});
@@ -21,6 +30,67 @@ class _MovieDetailsViewState extends ConsumerState<MovieDetailsView> {
   @override
   Widget build(BuildContext context) {
     MovieDetailsViewModel controller = ref.watch(movieDetailsViewModelProvider);
-    return Container();
+    return Scaffold(
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          MovieDetailAppBar(
+            poster: '${ApiUrls.imageBaseUrl}${controller.movie.posterPath}',
+            releaseDate:
+                'In Theatres ${DateFormat('MMM dd, yyyy').format(controller.movie.releaseDate)}',
+            onWatchTrailer: () {
+              print('hiii');
+            },
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextWidgets.headingWidget('Genres'),
+                  10.verticalSpace,
+                  Wrap(
+                    spacing: 7,
+                    runSpacing: 10,
+                    children: controller.movie.genres.map((e) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(55),
+                          color: Colors.lightGreen,
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 5,
+                          horizontal: 15,
+                        ),
+                        child: TextWidgets.generalText(e.name),
+                      );
+                    }).toList(),
+                  ),
+                  10.verticalSpace,
+                  Divider(
+                    color: Colors.grey[300],
+                  ),
+                  15.verticalSpace,
+                  TextWidgets.headingWidget('Overview'),
+                  10.verticalSpace,
+                  TextWidgets.generalText(
+                    controller.movie.overview,
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+            ),
+          )
+          // SliverList.builder(
+          //   itemBuilder: (context, index) {
+          //     return ListTile(
+          //       title: TextWidgets.generalText('text: $index'),
+          //     );
+          //   },
+          // )
+        ],
+      ),
+    );
   }
 }
--- lib/presentation/movie details/movie_details_vm.dart
+++ lib/presentation/movie details/movie_details_vm.dart
@@ -1,5 +1,8 @@
 import 'package:cowlar_task/core/injection/dependency_injection.dart';
+import 'package:cowlar_task/data/models/movie_detail_model.dart';
+import 'package:cowlar_task/data/models/response_model.dart';
 import 'package:cowlar_task/domain/usecases/get_movie_details.dart';
+import 'package:cowlar_task/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -10,13 +13,35 @@ final movieDetailsViewModelProvider = ChangeNotifierProvider((ref) {
 class MovieDetailsViewModel extends ChangeNotifier {
   final GetMovieDetails _getMovieDetails;
   MovieDetailsViewModel(this._getMovieDetails);
+
+  bool isLoading = false;
+  late MovieDetailModel movie;
   late int movieId;
+
   void init(int id) {
     movieId = id;
+    isLoading = true;
     fetchMovieDetails();
   }
 
-  fetchMovieDetails() {
-    _getMovieDetails(params: movieId);
+  fetchMovieDetails() async {
+    ResponseModel<MovieDetailModel> movieDetail = await _getMovieDetails(
+      params: movieId,
+    );
+    if (movieDetail.hasError) {
+      Utils.showSnackbar(message: movieDetail.errorMessage);
+      updateLoading(false);
+      return;
+    }
+    if (movieDetail.data != null) {
+      movie = movieDetail.data!;
+    }
+
+    updateLoading(false);
+  }
+
+  updateLoading(bool val) {
+    isLoading = val;
+    notifyListeners();
   }
 }
