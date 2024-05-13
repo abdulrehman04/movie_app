23784991e5d57ff51b380f5204fe9d4cbfed39a5
abdulrehman04 <abdulrehman71254@gmail.com>
2024-05-13 05:24:22 +0500

    setup video player

--- lib/presentation/video player/video_player.dart
+++ lib/presentation/video player/video_player.dart
@@ -1,14 +1,58 @@
 import 'package:cowlar_task/presentation/video%20player/video_player_vm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class VideoPlayerView extends ConsumerWidget {
-  const VideoPlayerView({super.key, required this.videoUrl});
-  final String videoUrl;
+class VideoPlayerView extends ConsumerStatefulWidget {
+  const VideoPlayerView({super.key, required this.movieId});
+  final int movieId;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _VideoPlayerViewState();
+}
+
+class _VideoPlayerViewState extends ConsumerState<VideoPlayerView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(videoPlayerViewModelProvider).init(widget.movieId);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     VideoPlayerViewModel controller = ref.watch(videoPlayerViewModelProvider);
-    return Container();
+    return Scaffold(
+      body: PopScope(
+        canPop: !controller.youtubeController.value.isFullScreen,
+        onPopInvoked: (didPop) {
+          if (controller.youtubeController.value.isFullScreen) {
+            controller.youtubeController.toggleFullScreenMode();
+            context.pop();
+          }
+        },
+        child: controller.isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : YoutubePlayer(
+                controller: controller.youtubeController,
+                showVideoProgressIndicator: true,
+                progressIndicatorColor: Colors.amber,
+                progressColors: const ProgressBarColors(
+                  playedColor: Colors.amber,
+                  handleColor: Colors.amberAccent,
+                ),
+                onEnded: (metaData) {
+                  if (controller.youtubeController.value.isFullScreen) {
+                    controller.youtubeController.toggleFullScreenMode();
+                  }
+                  context.pop();
+                },
+              ),
+      ),
+    );
   }
 }
--- lib/presentation/video player/video_player_vm.dart
+++ lib/presentation/video player/video_player_vm.dart
@@ -1,8 +1,68 @@
+import 'package:cowlar_task/core/injection/dependency_injection.dart';
+import 'package:cowlar_task/data/models/movie_detail_model.dart';
+import 'package:cowlar_task/data/models/movie_videos_model.dart';
+import 'package:cowlar_task/data/models/response_model.dart';
+import 'package:cowlar_task/domain/usecases/get_movie_videos.dart';
+import 'package:cowlar_task/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 final videoPlayerViewModelProvider = ChangeNotifierProvider((ref) {
-  return VideoPlayerViewModel();
+  return VideoPlayerViewModel(getIt());
 });
 
-class VideoPlayerViewModel extends ChangeNotifier {}
+class VideoPlayerViewModel extends ChangeNotifier {
+  final GetMovieVideos _getMovieVideos;
+  VideoPlayerViewModel(this._getMovieVideos);
+
+  late YoutubePlayerController youtubeController;
+  bool isLoading = false;
+  late int movieId;
+  late MovieVideosModel videos;
+
+  Future<void> init(int id) async {
+    isLoading = true;
+    movieId = id;
+    await fetchVideos();
+    print('here');
+    initYoutube(getTrailerId());
+  }
+
+  Future<void> fetchVideos() async {
+    ResponseModel<MovieVideosModel> movieDetail = await _getMovieVideos(
+      params: movieId,
+    );
+    if (movieDetail.hasError) {
+      Utils.showSnackbar(message: movieDetail.errorMessage);
+      updateLoading(false);
+      return;
+    }
+    if (movieDetail.data != null) {
+      videos = movieDetail.data!;
+    }
+    updateLoading(false);
+  }
+
+  getTrailerId() {
+    return videos.results
+        .firstWhere((element) => element.type == Type.TRAILER)
+        .key;
+  }
+
+  initYoutube(String videoId) {
+    youtubeController = YoutubePlayerController(
+      initialVideoId: videoId,
+      flags: const YoutubePlayerFlags(
+        autoPlay: true,
+        mute: true,
+      ),
+    );
+    youtubeController.toggleFullScreenMode();
+  }
+
+  updateLoading(bool val) {
+    isLoading = val;
+    notifyListeners();
+  }
+}
