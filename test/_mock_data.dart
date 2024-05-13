3e81525582b794eead1d63f7701cb4c066691125
abdulrehman04 <abdulrehman71254@gmail.com>
2024-05-13 20:26:15 +0500

    Added mock responses

---
+++ test/_mock_data.dart
@@ -0,0 +1,241 @@
+part of './widget_test.dart';
+
+Map<String, dynamic> dummyMoviDetailseResponse = {
+  "adult": false,
+  "backdrop_path": "/ySgY4jBvZ6qchrxKnBg4M8tZp8V.jpg",
+  "belongs_to_collection": null,
+  "budget": 28000000,
+  "genres": [
+    {"id": 27, "name": "Horror"},
+    {"id": 53, "name": "Thriller"}
+  ],
+  "homepage": "https://www.abigailmovie.com",
+  "id": 1111873,
+  "imdb_id": "tt27489557",
+  "origin_country": ["US"],
+  "original_language": "en",
+  "original_title": "Abigail",
+  "overview":
+      "A group of criminals kidnaps a teenage ballet dancer, the daughter of a notorious gang leader, in order to obtain a ransom of 50 million, but over time, they discover that she is not just an ordinary girl. After the kidnappers begin to diminish, one by one, they discover, to their increasing horror, that they are locked inside with an unusual girl.",
+  "popularity": 1626.563,
+  "poster_path": "/7qxG0zyt29BI0IzFDfsps62kbQi.jpg",
+  "production_companies": [
+    {
+      "id": 130448,
+      "logo_path": "/yHWTTGKbOGZKUd1cp6l3uLyDeiv.png",
+      "name": "Project X Entertainment",
+      "origin_country": "US"
+    },
+    {
+      "id": 126588,
+      "logo_path": "/cNhOITS96oOV7SCgUHxvZlWRecx.png",
+      "name": "Radio Silence",
+      "origin_country": "US"
+    },
+    {
+      "id": 33,
+      "logo_path": "/8lvHyhjr8oUKOOy2dKXoALWKdp0.png",
+      "name": "Universal Pictures",
+      "origin_country": "US"
+    },
+    {
+      "id": 19367,
+      "logo_path": null,
+      "name": "Vinson Films",
+      "origin_country": "US"
+    }
+  ],
+  "production_countries": [
+    {"iso_3166_1": "US", "name": "United States of America"}
+  ],
+  "release_date": "2024-04-18",
+  "revenue": 28536335,
+  "runtime": 109,
+  "spoken_languages": [
+    {"english_name": "English", "iso_639_1": "en", "name": "English"}
+  ],
+  "status": "Released",
+  "tagline": "Children can be such monsters.",
+  "title": "Abigail",
+  "video": false,
+  "vote_average": 7.037,
+  "vote_count": 283
+};
+
+Map<String, dynamic> dummyVideoResponse = {
+  "id": 1111873,
+  "results": [
+    {
+      "iso_639_1": "en",
+      "iso_3166_1": "US",
+      "name": "You've Made A Mistake - Extended Preview",
+      "key": "S6BPb9PQqCk",
+      "site": "YouTube",
+      "size": 1080,
+      "type": "Clip",
+      "official": true,
+      "published_at": "2024-05-07T17:00:25.000Z",
+      "id": "663aa69823b085be4e0ac52d"
+    },
+    {
+      "iso_639_1": "en",
+      "iso_3166_1": "US",
+      "name": "Watch at Home Now",
+      "key": "9CpxLW33RL8",
+      "site": "YouTube",
+      "size": 1080,
+      "type": "Teaser",
+      "official": true,
+      "published_at": "2024-05-06T13:00:07.000Z",
+      "id": "663aa68adcd4ae478cd73e93"
+    },
+    {
+      "iso_639_1": "en",
+      "iso_3166_1": "US",
+      "name": "My Universal Story: Julio Quijada",
+      "key": "847dOjhzDws",
+      "site": "YouTube",
+      "size": 1080,
+      "type": "Behind the Scenes",
+      "official": true,
+      "published_at": "2024-04-19T19:00:08.000Z",
+      "id": "662395b4258823017d90bd67"
+    },
+    {
+      "iso_639_1": "en",
+      "iso_3166_1": "US",
+      "name": "Melissa on Joey",
+      "key": "H2gH3eRHFTw",
+      "site": "YouTube",
+      "size": 1080,
+      "type": "Featurette",
+      "official": true,
+      "published_at": "2024-04-17T23:00:09.000Z",
+      "id": "6622c49d8a88b20185cbcb6d"
+    },
+    {
+      "iso_639_1": "en",
+      "iso_3166_1": "US",
+      "name": "\"Abigail In Action\" Featurette",
+      "key": "QCZoCajfHTI",
+      "site": "YouTube",
+      "size": 1080,
+      "type": "Featurette",
+      "official": true,
+      "published_at": "2024-04-15T11:42:09.000Z",
+      "id": "66239661cb6db50163ad58e7"
+    },
+    {
+      "iso_639_1": "en",
+      "iso_3166_1": "US",
+      "name": "In Theaters Friday",
+      "key": "AFMizvXY6bo",
+      "site": "YouTube",
+      "size": 1080,
+      "type": "Teaser",
+      "official": true,
+      "published_at": "2024-04-11T17:09:19.000Z",
+      "id": "661d7296cb6db5014708f94a"
+    },
+    {
+      "iso_639_1": "en",
+      "iso_3166_1": "US",
+      "name": "\"Kathryn On Sammy\" Featurette",
+      "key": "E0kmxgaSvF8",
+      "site": "YouTube",
+      "size": 1080,
+      "type": "Featurette",
+      "official": true,
+      "published_at": "2024-04-11T14:25:27.000Z",
+      "id": "661836ec967cc701643aa696"
+    },
+    {
+      "iso_639_1": "en",
+      "iso_3166_1": "US",
+      "name": "It's A Blood Bath",
+      "key": "kD3hXYEZghw",
+      "site": "YouTube",
+      "size": 1080,
+      "type": "Behind the Scenes",
+      "official": true,
+      "published_at": "2024-04-04T19:44:54.000Z",
+      "id": "6610d90019690c017ca330f1"
+    },
+    {
+      "iso_639_1": "en",
+      "iso_3166_1": "US",
+      "name": "Official Trailer 2",
+      "key": "xtAL2x58hns",
+      "site": "YouTube",
+      "size": 1080,
+      "type": "Trailer",
+      "official": true,
+      "published_at": "2024-04-04T17:00:42.000Z",
+      "id": "6610d939d768fe01624180ff"
+    },
+    {
+      "iso_639_1": "en",
+      "iso_3166_1": "US",
+      "name": "Take a BTS Tour of the Abigail set with Kathryn Newton",
+      "key": "dq5Zf8pXVl4",
+      "site": "YouTube",
+      "size": 1080,
+      "type": "Behind the Scenes",
+      "official": true,
+      "published_at": "2024-03-27T17:06:59.000Z",
+      "id": "6610d9076f43ec017dc159f0"
+    },
+    {
+      "iso_639_1": "en",
+      "iso_3166_1": "US",
+      "name": "Official UK Trailer",
+      "key": "yincrFV0P8s",
+      "site": "YouTube",
+      "size": 1080,
+      "type": "Trailer",
+      "official": true,
+      "published_at": "2024-01-11T16:00:41.000Z",
+      "id": "6616aee06af9dd017dedc479"
+    },
+    {
+      "iso_639_1": "en",
+      "iso_3166_1": "US",
+      "name": "Official Trailer",
+      "key": "3PsP8MFH8p0",
+      "site": "YouTube",
+      "size": 1080,
+      "type": "Trailer",
+      "official": true,
+      "published_at": "2024-01-11T16:00:08.000Z",
+      "id": "65a01176c14fee01216d82fe"
+    }
+  ]
+};
+
+Map<String, dynamic> dummyUpcomingMovie = {
+  "adult": false,
+  "backdrop_path": "/v5XyXZe8FADw8iHupB4L7QOAwH9.jpg",
+  "genre_ids": [16, 35, 10751],
+  "id": 748783,
+  "original_language": "en",
+  "original_title": "The Garfield Movie",
+  "overview":
+      "Garfield, the world-famous, Monday-hating, lasagna-loving indoor cat, is about to have a wild outdoor adventure! After an unexpected reunion with his long-lost father – scruffy street cat Vic – Garfield and his canine friend Odie are forced from their perfectly pampered life into joining Vic in a hilarious, high-stakes heist.",
+  "popularity": 671.405,
+  "poster_path": "/zK2sFxZcelHJRPVr242rxy5VK4T.jpg",
+  "release_date": "2024-04-30",
+  "title": "The Garfield Movie",
+  "video": false,
+  "vote_average": 6.5,
+  "vote_count": 52
+};
+
+Map<String, dynamic> dummyUpcomingMovieResponse = {
+  "dates": {"maximum": "2024-06-05", "minimum": "2024-05-15"},
+  "page": 1,
+  "results": [
+    dummyUpcomingMovie,
+  ],
+  "total_pages": 51,
+  "total_results": 1018
+};
